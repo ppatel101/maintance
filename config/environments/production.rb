01273d60bd45e7ff0c4bd2014f79f55240a393a1
ppatel101 <parth.logicalstreet@gmail.com>
2019-01-18 14:52:27 +0530

    make it better

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,9 +31,10 @@ Rails.application.configure do
   end
   config.sass.style = :compressed
   # config.assets.css_compressor = :sass
+  config.assets.compile = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
